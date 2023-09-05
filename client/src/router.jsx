d6c3c6c7dcc39197c5a21af81649a1ab931db16d
uxmoon <uxmoon@gmail.com>
2023-09-05 11:49:20 -0300

    Add default path

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { Navigate, createBrowserRouter } from 'react-router-dom';
 import { PostList } from './pages/PostList';
 import { UserList } from './pages/UserList';
 import { TodoList } from './pages/TodoList';
@@ -9,6 +9,7 @@ export const router = createBrowserRouter([
     path: '/',
     element: <RootLayout />,
     children: [
+      { index: true, element: <Navigate to='/posts' /> },
       { path: 'posts', element: <PostList /> },
       { path: 'users', element: <UserList /> },
       { path: 'todos', element: <TodoList /> },
