6167d995d4f35a600cf61dc7eb7e014d03db75bf
uxmoon <uxmoon@gmail.com>
2023-09-05 16:43:59 -0300

    Render users grid

--- client/src/pages/UserList.jsx
+++ client/src/pages/UserList.jsx
@@ -1,10 +1,44 @@
-import { Heading } from '@chakra-ui/react';
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Heading,
+  SimpleGrid,
+  Link as ChakraLink,
+  ListItem,
+  List,
+} from '@chakra-ui/react';
+import { Link as ReactRouterLink } from 'react-router-dom';
 import { useLoaderData } from 'react-router-dom';
 import { getUsers } from '../api/users';
 
 function UserList() {
   const users = useLoaderData();
-  return <Heading>User List: {users.length}</Heading>;
+  return (
+    <>
+      <Heading>User List: {users.length}</Heading>
+      <SimpleGrid columns={2} spacing={10}>
+        {users.map((user) => (
+          <Card key={user.id}>
+            <CardHeader>{user.name}</CardHeader>
+            <CardBody>
+              <List>
+                <ListItem>{user.company.name}</ListItem>
+                <ListItem>{user.website}</ListItem>
+                <ListItem>{user.email}</ListItem>
+              </List>
+            </CardBody>
+            <CardFooter>
+              <ChakraLink as={ReactRouterLink} to={user.id.toString()}>
+                View
+              </ChakraLink>
+            </CardFooter>
+          </Card>
+        ))}
+      </SimpleGrid>
+    </>
+  );
 }
 
 function loader({ request: { signal } }) {
