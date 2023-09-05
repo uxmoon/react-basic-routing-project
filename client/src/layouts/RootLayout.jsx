eec5d4ee627dcd1d3f3af48418097240f3136810
uxmoon <uxmoon@gmail.com>
2023-09-05 11:58:31 -0300

    Add scroll restoration

--- client/src/layouts/RootLayout.jsx
+++ client/src/layouts/RootLayout.jsx
@@ -1,5 +1,26 @@
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet, ScrollRestoration } from 'react-router-dom';
 
 export function RootLayout() {
-  return <Outlet />;
+  return (
+    <>
+      <nav className='top-nav'>
+        <div className='nav-text-large'>My App</div>
+        <ul className='nav-list'>
+          <li>
+            <Link to='/posts'>Posts</Link>
+          </li>
+          <li>
+            <Link to='/users'>Users</Link>
+          </li>
+          <li>
+            <Link to='/todos'>Todos</Link>
+          </li>
+        </ul>
+      </nav>
+      <ScrollRestoration />
+      <div className='container'>
+        <Outlet />
+      </div>
+    </>
+  );
 }
