f32e3361afb9c38579a817ea27a10202c5bda04d
uxmoon <uxmoon@gmail.com>
2023-09-05 15:53:52 -0300

    Display posts using Cards

--- client/src/pages/PostList.jsx
+++ client/src/pages/PostList.jsx
@@ -1,10 +1,36 @@
-import { Heading } from '@chakra-ui/react';
+import {
+  Heading,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  SimpleGrid,
+  Link as ChakraLink,
+} from '@chakra-ui/react';
+import { Link as ReactRouterLink } from 'react-router-dom';
 import axios from 'axios';
 import { useLoaderData } from 'react-router-dom';
 
 function PostList() {
   const posts = useLoaderData();
-  return <Heading>Post List: {posts.length}</Heading>;
+  return (
+    <>
+      <Heading>Post List: {posts.length}</Heading>
+      <SimpleGrid columns={2} spacing={10}>
+        {posts.map((post) => (
+          <Card key={post.id}>
+            <CardHeader>{post.title}</CardHeader>
+            <CardBody>{post.body}</CardBody>
+            <CardFooter>
+              <ChakraLink as={ReactRouterLink} to={`/posts/${post.id}`}>
+                View
+              </ChakraLink>
+            </CardFooter>
+          </Card>
+        ))}
+      </SimpleGrid>
+    </>
+  );
 }
 
 function loader({ request: { signal } }) {
