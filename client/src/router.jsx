ebaca4f1c70557698f9385133b3d6a425bded9af
uxmoon <uxmoon@gmail.com>
2023-09-06 11:17:24 -0300

    Add error message

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -5,31 +5,39 @@ import { todoListRoute } from './pages/TodoList';
 import { RootLayout } from './layouts/RootLayout';
 import { postRoute } from './pages/Post';
 import { userRoute } from './pages/User';
+import { Heading } from '@chakra-ui/react';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
+    errorElement: <h1>Error</h1>,
     children: [
-      { index: true, element: <Navigate to='/posts' /> },
       {
-        path: 'posts',
+        errorElement: <Heading>Error</Heading>,
         children: [
+          { index: true, element: <Navigate to='/posts' /> },
           {
-            index: true,
-            ...postListRoute,
+            path: 'posts',
+            children: [
+              {
+                index: true,
+                ...postListRoute,
+              },
+              { path: ':postId', ...postRoute },
+            ],
           },
-          { path: ':postId', ...postRoute },
+          {
+            path: 'users',
+            children: [
+              { index: true, ...userListRoute },
+              { path: ':userId', ...userRoute },
+            ],
+          },
+          { path: 'todos', ...todoListRoute },
+          { path: '*', element: <h1>404 - page not found</h1> },
         ],
       },
-      {
-        path: 'users',
-        children: [
-          { index: true, ...userListRoute },
-          { path: ':userId', ...userRoute },
-        ],
-      },
-      { path: 'todos', ...todoListRoute },
     ],
   },
 ]);
